701c5bf1fa916848948e1e70d5e87795fde05ecc
vish202324 <143012163+vish202324@users.noreply.github.com>
2024-10-27 20:39:06 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,19 +6,15 @@ public class Main {
         // Adding sample questions
         quiz.addQuestion(new Question(
             "What is the capital of France?", 
-            new String[]{"Berlin", "Madrid", "Paris", "Rome"}, 
-            2
-        ), 0);
+            new String[]{"Berlin", "Madrid", "Paris", "Rome"}, 2), 0);
+        
         quiz.addQuestion(new Question(
             "Which language is used for Android development?", 
-            new String[]{"Swift", "Kotlin", "JavaScript", "Python"}, 
-            1
-        ), 1);
+            new String[]{"Swift", "Kotlin", "JavaScript", "Python"}, 1), 1);
+        
         quiz.addQuestion(new Question(
             "What is 2 + 2?", 
-            new String[]{"3", "4", "5", "6"}, 
-            1
-        ), 2);
+            new String[]{"3", "4", "5", "6"}, 1), 2);
 
         // Start the quiz
         quiz.startQuiz();
